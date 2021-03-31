2f352600efdcdb68392a02c57faa2fd48a27df1c
Kiryl Krasouski <ziksow@gmail.com>
2021-03-31 22:16:47 +0300

    refactor: delete tag selector in app.js according to rule 3.4

--- app.js
+++ app.js
@@ -135,7 +135,7 @@ addButton.addEventListener("click",ajaxRequest);
 var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
   console.log("bind list item events");
   //select ListItems children
-  var checkBox=taskListItem.querySelector("input[type=checkbox]");
+  var checkBox=taskListItem.querySelector(".done-cheker");
   var editButton=taskListItem.querySelector(".button_edit");
   var deleteButton=taskListItem.querySelector(".button_delete");
   //Bind editTask to edit button.
